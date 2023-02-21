701c7c5fa6c9a86d608bd54f48db55b85184e0fa
norkov1994 <user_317215@icloud.com>
2023-02-21 11:43:13 +0300

    folder for lesson 5 task 1 added

--- lesson-5/Program.cs
+++ lesson-5/Program.cs
@@ -49,53 +49,12 @@ void les5_task1()
 // Написать программу масштабирования фигуры
 void les5_task2()
 {
-    //Создание объекта для генерации чисел
-    Random rnd = new Random();
-    int [] array = new int[100];
-    int evenNumQty = 0;
-    int notEvenNumQty = 0;
 
-    //заполнение массива случайными трехзначными числами и подсчет четных и нечетных значений
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = rnd.Next(100, 999);
-        if (array[i] % 2 == 0) evenNumQty ++;
-        else notEvenNumQty ++;
-    }
-
-    Console.WriteLine($"Количество четных чисел: {evenNumQty}");
-    Console.WriteLine($"Количество нечетных чисел: {notEvenNumQty}");
 }
 
 // Написать программу копирования массива.
 void les5_task3()
 {
-    //Создание объекта для генерации чисел
-    Random rnd = new Random();
-    int [] array = new int[123];
 
-    Console.WriteLine("Введите числа через запятую, пример - (12, 32)");
-    // создаем переменную массива строк и в нее добавляем массив строк разделенных запятой
-    string[] temp = Console.ReadLine().Split(',');
-    int a = int.Parse(temp[0]);
-    int b = int.Parse(temp[1]);
-    int aCounter = 0;
-    int bCounter = 0;
-
-    //заполнение массива случайными трехзначными числами и суммирование значений стоящих на нечетной позиции
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = rnd.Next(1, 100);
-    }
-
-    //проверяем сколько раз встречается число первое введенное число и второе
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] == a) aCounter ++;
-        if (array[i] == b) bCounter ++;
-    }
-
-    Console.WriteLine($"Число {a} встречается в массиве: {aCounter} раз");
-    Console.WriteLine($"Число {b} встречается в массиве: {bCounter} раз");
 
 }
