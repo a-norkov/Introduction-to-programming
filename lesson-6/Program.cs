ca54c2d57762bca5bc75f00189e7b4474878783c
norkov1994 <user_317215@icloud.com>
2023-02-26 17:41:41 +0300

    folder for lesson 6 added

---
+++ lesson-6/Program.cs
@@ -0,0 +1,48 @@
+﻿// Seminar 6
+Console.Clear();
+Console.WriteLine("Выберите вариант задачи:\n\n" 
++ " 1 - Показать двумерный массив размером m×n заполненный вещественными числами.\n" 
++ " 2 - Задать двумерный массив следующим правилом: Aₘₙ = m+n.\n"
++ " 3 - В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты.\n"
++ " 4 - Написать программу, которая обменивает элементы первой строки и последней строки.\n");
+
+int userVar = int.Parse(Console.ReadLine());
+
+switch (userVar)
+{
+    case 1:
+        les6_task1();
+        break;
+    case 2:
+        les6_task2();
+        break;
+    case 3:
+        les6_task3();
+        break;
+    case 4:
+        les6_task4();
+        break;
+    default:
+        Console.WriteLine("Введеное число не соответсвует возможным вариантам");
+        break;
+}
+
+void les6_task1()
+{
+
+}
+
+void les6_task2()
+{
+
+}
+
+void les6_task3()
+{
+
+}
+
+void les6_task4()
+{
+
+}
