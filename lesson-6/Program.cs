12c59347a7bb8c8cd08a62befdc18f906f50103e
norkov1994 <user_317215@icloud.com>
2023-02-26 19:48:05 +0300

    code for lesson 6 added

--- lesson-6/Program.cs
+++ lesson-6/Program.cs
@@ -27,22 +27,121 @@ switch (userVar)
         break;
 }
 
+//Показать двумерный массив размером m×n заполненный вещественными числами.
 void les6_task1()
 {
+    Console.WriteLine("Введите m:");
+    int m = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите n:");
+    int n = int.Parse(Console.ReadLine()); 
+    double [,] numbers = new double [m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            numbers[i, j] = rnd.NextDouble() + rnd.Next(1, 10);
+            Console.Write($"{numbers[i, j].ToString("0#.#0")} | ");
+        }
+
+        Console.Write("\n");
+    }
 
 }
 
+//Задать двумерный массив следующим правилом: Aₘₙ = m+n.
 void les6_task2()
 {
+    Console.WriteLine("Введите m:");
+    int m = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите n:");
+    int n = int.Parse(Console.ReadLine()); 
+    int [,] numbers = new int[m, n];
+    //Random rnd = new Random();
 
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            numbers[i, j] = i + j;
+            Console.Write($"{numbers[i, j].ToString("0#.#0")} | ");
+        }
+
+        Console.Write("\n");
+    }
 }
 
+//В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты.
 void les6_task3()
 {
 
+    int [,] numbers = new int [10, 10];
+    Random rnd = new Random();
+
+    Console.WriteLine("Исходный массив:");
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            numbers[i, j] = rnd.Next(1, 10);
+            Console.Write($"{numbers[i, j].ToString("0#.#0")} | ");
+            if (i % 2 == 0 && j % 2 == 0 && i != 0 && j != 0)
+            {
+                numbers[i, j] = numbers[i, j] * numbers[i, j];
+            }
+        }
+
+        Console.Write("\n");
+    }
+
+    Console.WriteLine("Обработанный массив:");
+        for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            Console.Write($"{numbers[i, j].ToString("0#.#0")} | ");
+        }
+
+        Console.Write("\n");
+    }
+
 }
 
+//Написать программу, которая обменивает элементы первой строки и последней строки.
 void les6_task4()
 {
+    int [,] numbers = new int [10, 10];
+    int tmp = new int();
+    Random rnd = new Random();
 
+    Console.WriteLine("Исходный массив:");
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            numbers[i, j] = rnd.Next(1, 10);
+            Console.Write($"{numbers[i, j].ToString("0#.#0")} | ");
+        }
+
+        Console.Write("\n");
+    }
+
+    Console.WriteLine("Обработанный массив:");
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        tmp = numbers[0, i];
+        numbers[0, i] = numbers[9, i];
+        numbers[9, i] = tmp;
+    }
+
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            Console.Write($"{numbers[i, j].ToString("0#.#0")} | ");
+        }
+
+        Console.Write("\n");
+    }
 }
