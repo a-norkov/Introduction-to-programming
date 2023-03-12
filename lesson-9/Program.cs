26a2b9ee3b305f8b2f2b9820b59235acd7c5e6db
Aleksei Norkov <user_317215@icloud.com>
2023-03-12 13:29:29 +0300

    added solution for task 4 lesson 9

--- lesson-9/Program.cs
+++ lesson-9/Program.cs
@@ -99,7 +99,6 @@ void les9_task3()
 {
     Console.WriteLine("Введите число:");
     int number = int.Parse(Console.ReadLine());
-    int sum = new int();
 
     Console.WriteLine($"Сумма чисел равна: {numberSum(number)}");
 
@@ -121,7 +120,34 @@ void les9_task3()
 //Написать программу вычисления функции Аккермана.
 void les9_task4()
 {
+    Console.WriteLine("Введите число M:");
+    int m = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите число N:");
+    int n = int.Parse(Console.ReadLine());
 
+    Console.WriteLine($"Функция Аккермана A({m}, {n}) : {result(m, n)}");
+
+
+    int result(int n, int m)
+    {
+
+        if (n == 0) 
+        {
+            return m + 1;
+        }
+        
+        else if (n != 0 && m == 0) 
+        {
+            return result(n - 1, 1);
+        }
+        
+        else if (n > 0 && m > 0) 
+        {
+            return result(n - 1, result(n, m - 1));
+        }
+        
+        return result(n,m);
+    }
 }
 
 //Написать программу возведения числа А в целую стень B.
